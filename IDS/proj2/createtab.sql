39245d1e4e360ee7dc677f7d4f24be15810e444c
Leachim93 <xklcom00@stud.fit.vutbr.cz>
2015-03-22 21:20:39 +0100

    Modified createtab.sql

--- IDS/proj2/createtab.sql
+++ IDS/proj2/createtab.sql
@@ -13,8 +13,8 @@ CREATE TABLE "Order"(
 "Deadline"          DATE,
 "Delivery"          VARCHAR(20) NOT NULL,
 "Delivery_Adress"   VARCHAR(50),
-"IsProcessed"       VARCHAR(3) NOT NULL,
-"CostumerID_Costumer" INTEGER
+"IsProcessed"       VARCHAR(3) DEFAULT 'no',
+"CustomerID_Customer" INTEGER
 );
 
 CREATE TABLE "OrderedBread"(
@@ -24,18 +24,18 @@ CREATE TABLE "OrderedBread"(
 );
 
 CREATE TABLE "Bread"(
-"ID_Bread"          INTEGER,
+"ID_Bread"          INTEGER NOT NULL,
 "Name"              VARCHAR(20) NOT NULL,
 "Cost"              DECIMAL(8,2)
 );
 
 CREATE TABLE "Employee"(
-"ID_PersonalNumber"   INTEGER,
-"BirthNumber"         CHAR(11) NOT NULL,
+"ID_PersonalNumber"   INTEGER NOT NULL,
+"BirthNumber"         CHAR(16) NOT NULL,
 "Name"                VARCHAR(50),
 "BirthDate"           DATE,
-"Adress"              VARCHAR(20),
-"AccountNumber"       VARCHAR(15),
+"Adress"              VARCHAR(50),
+"AccountNumber"       VARCHAR(16),
 "ContractType"        VARCHAR(20),
 "Salary"              INTEGER
 );
@@ -60,7 +60,6 @@ CREATE TABLE "Delivery_Area"(
 "ID_Area"                   INTEGER,
 "Name"                      VARCHAR(20),
 "StreetList"                VARCHAR(200)
-
 );
 
 
@@ -80,3 +79,30 @@ ALTER TABLE "LogisticManager" ADD FOREIGN KEY ("EmployeeID_PersonalNumber") REFE
 ALTER TABLE "Driver" ADD FOREIGN KEY ("EmployeeID_PersonalNumber") REFERENCES "Employee"("ID_PersonalNumber");
 ALTER TABLE "Driver" ADD FOREIGN KEY ("AreaID_area") REFERENCES "Delivery_Area"("ID_Area");
 ALTER TABLE "Baker" ADD FOREIGN KEY ("EmployeeID_PersonalNumber") REFERENCES "Employee"("ID_PersonalNumber");
+
+ALTER TABLE "Employee" ADD CONSTRAINT "chck_birth_number" CHECK (regexp_like("BirthNumber", '[0-9]{2}[01235678][0-9][0-3][0-9][/][0-9]{4}') );
+
+-- INSERTING
+INSERT INTO "Delivery_Area" ("ID_Area", "Name", "StreetList")
+VALUES(1, 'Technologický park', 'Kolejní, Purkyòova, Èervinkova');
+INSERT INTO "Delivery_Area" ("ID_Area", "Name", "StreetList")
+VALUES(2, 'Cejl', 'Soudní, Valcha, Tkalcovská');
+
+-- Bread
+INSERT INTO "Bread" ("ID_Bread", "Name", "Cost")
+VALUES (1, 'Chleba bíly', 20.50);
+INSERT INTO "Bread" ("ID_Bread", "Name", "Cost")
+VALUES (2, 'Rohlík grahamový', 6.50);
+INSERT INTO "Bread" ("ID_Bread", "Name", "Cost")
+VALUES (3, 'Buchta s lekvárem', 12);
+
+-- Employees
+INSERT INTO "Employee" 
+VALUES (100, '990101/1234', 'Michal Novotný', '1.1.1999','Kolejni 6 Brno 602 00','1234567891234567','full-time', '15000');
+INSERT INTO "LogisticManager"
+VALUES (100, 122);
+INSERT INTO "Employee" 
+VALUES (200, '991010/1234', 'Ladislav Pochmúrny', '10.10.1999','6. apríla 333 Vrbové 922 03 SR','1234567891231234','full-time', '15000');
+INSERT INTO "Baker"
+VALUES (200, 'Hlavní pìc');
+--INSERT INTO "Order" ("Date", "Deadline", "Delivery", "DeliveryAdress")
