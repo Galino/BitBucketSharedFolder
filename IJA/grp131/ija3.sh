45c9c523efe021ab4d709ac7cb788c5de8100f29
Leachim93 <xklcom00@stud.fit.vutbr.cz>
2015-04-12 18:52:48 +0200

    Ija3.sh modified, this version was uploaded as final version of Homework3

--- IJA/grp131/ija3.sh
+++ IJA/grp131/ija3.sh
@@ -1,3 +1,4 @@
+ulimit -v unlimited
 javac -d build/ src/ija/homework3/Homework3.java src/ija/homework2/board/*
 jar -cvfm dest-client/Homework3.jar manifest.mf  -C build ija
 java -jar dest-client/Homework3.jar 
